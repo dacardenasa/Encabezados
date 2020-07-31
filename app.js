2f6b4605fb7705238ecb0a11d5f0bd59210dc225
dacardenasa <diego.cardenasaleg@gmail.com>
2020-07-31 12:28:16 -0500

    Update const name browser to browserName in App.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  const browser = req.header('User-Agent');
-  res.send(browser);
+  const browserName = req.header('User-Agent');
+  res.send(browserName);
 })
 
 app.listen(port, ()=> console.log('Listening on port 3000!'));
